57628fa342e64fd300bb12cf6039dc0b8095a676
tarek008 <tarek.ayadi@esprit.tn>
2023-02-08 10:30:29 +0100

    Firstcommit

--- workshop-react0/src/App.js
+++ workshop-react0/src/App.js
@@ -2,22 +2,46 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const input=[["a","b","c"],["c","d","f"],["d","f","g"]]  
+const obj={}
+
+const occurence=(tab)=>{
+  tab.map((each)=>{
+  if (obj[each]){
+    obj[each]+=1
+  }else{
+    obj[each]=1
+  }  
+   
+})
+   return obj
+}
+
+
+
+let students=[{name:'John',id:123,marks:98},
+{name:'Baba',id:101,marks:23},
+{name:'John',id:200,marks:45},
+{name:'Wick',id:115,marks:45}
+
+]
+const summ=0
+const res = students.map(function(input){
+ if(input.marks>50){
+  summ=summ+input.marks
+ }
+ return summ ;
+})
+
+const Tab={}
+Tab.push({nom:"Haythem"},{prenom:"Manoubi"})
+
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>Test:</p>
     </div>
   );
 }
