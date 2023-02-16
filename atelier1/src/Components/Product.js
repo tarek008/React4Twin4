74a6e6903175f581043c2a519fa2477600519054
tarek008 <tarek.ayadi@esprit.tn>
2023-02-16 12:15:29 +0100

    new update

--- atelier1/src/Components/Product.js
+++ atelier1/src/Components/Product.js
@@ -3,55 +3,29 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Alert } from "react-bootstrap";
 
-export default class Product extends Component{
-
-    constructor(props){
-        super(props);
-        this.state =  {like : this.props.product.like,quantity:this.props.product.quantity, alertVisible:false};
-        this.addLikes = this.addLikes.bind(this);
-        this.buyProducts = this.buyProducts.bind(this);
-        this.handleVisible = this.handleVisible.bind(this);
-    }
+const Product = ({product}) => {
+  return ( 
+    <div>
 
 
-    addLikes(){
-        this.setState((oldState)=>({
-            like : oldState.like +1
-        }));
-    }
-
-    buyProducts (){
-        this.setState((oldState)=>({
-            quantity:oldState.quantity-1
-        }));
-        this.handleVisible()
-    }   
-
-    handleVisible = () => { 
-        this.setState({alertVisible:true})
-        setTimeout(() => { 
-            this.setState({alertVisible:false})
-        }, 2000);
-    } 
-
-    render(){
-        return (<>
             <Card style={{ width: '25rem',minHeight:'630px' }}>
-            <Card.Img variant="top" src={require('../assets/images/'+this.props?.product.image)} />
+            <Card.Img variant="top" src={require('../assets/images/'+product.image)} />
             <Card.Body>
-              <Card.Title>{this.props.product.name}</Card.Title>
+              <Card.Title>{product.name}</Card.Title>
               <Card.Text>
-                {this.props.product.description}
+                {product.description}
               </Card.Text>
               <Card.Text>
-                {this.props.product.price} DT
+                {product.price} DT
               </Card.Text>
-              <Button variant="primary" onClick={this.addLikes} >Like</Button> <small>{this.state.like} Likes</small>
-              <Button variant="danger" onClick={this.buyProducts} disabled={this.state.quantity === 0}>Buy</Button> <small>{this.state.quantity} Products</small>
+              <Button variant="primary"  >Like</Button> <small>Likes</small>
+              <Button variant="danger" >Buy</Button> <small> Products</small>
             </Card.Body>
-            <Alert variant="primary" show={this.state?.alertVisible}  >you bought an Item !!!!</Alert>    
+            <Alert variant="primary"  >you bought an Item !!!!</Alert>    
           </Card>
-          </>
-        );
-    }
+         
+    </div>
+   );
 }
+ 
+export default Product;
