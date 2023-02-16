4518b5215dd2fcae69f85da8961e2a0b8fc7210c
tarek008 <tarek.ayadi@esprit.tn>
2023-02-16 12:22:39 +0100

    work in progress

--- atelier1/src/Components/Product.js
+++ atelier1/src/Components/Product.js
@@ -2,8 +2,19 @@ import { Component } from "react";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Alert } from "react-bootstrap";
+import React from "react";
 
 const Product = ({product}) => {
+
+
+  const [Like,SetLike] = React.useState(product.like);
+
+
+  const handleLike = () => {
+    SetLike(Like+1);
+  }
+
+
   return ( 
     <div>
 
@@ -18,7 +29,7 @@ const Product = ({product}) => {
               <Card.Text>
                 {product.price} DT
               </Card.Text>
-              <Button variant="primary"  >Like</Button> <small>Likes</small>
+              <Button variant="primary" onClick={handleLike} >Like</Button> <small>Likes: {Like}</small>
               <Button variant="danger" >Buy</Button> <small> Products</small>
             </Card.Body>
             <Alert variant="primary"  >you bought an Item !!!!</Alert>    
--- atelier1/src/Components/Products.js
+++ atelier1/src/Components/Products.js
@@ -5,34 +5,21 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Alert } from "react-bootstrap";
 import ProductsList from '../products.json';
-export default class Products extends Component{
-    constructor(props) {
-        super(props);
-        this.state ={alertVisible:false}
-        this.handleVisible = this.handleVisible.bind(this);
-    }
-    handleVisible = () => { 
-        this.setState({alertVisible:true})
-        setTimeout(() => { 
-            this.setState({alertVisible:false})
-        }, 3000);
-    } 
-    componentDidMount() {
-    this.handleVisible();
-    }
 
 
-    render(){
-        return (
-            <div>
-                <Alert variant="dark" show={this.state?.alertVisible}><Alert.Heading>Hey, Welcome To Our Shop <strong>MyStore</strong></Alert.Heading>
-                <p>Thank you for choosing our store, we hope you enjoy your shopping experience!</p></Alert>
-                <h1>Products</h1>
-                <Row>
-        {ProductsList.map((prod,i)=>(<Col key={i}><Product  product={prod}></Product></Col>))}
-      </Row>
-            </div>
-     
-        );
-    }
+const Products = () => {
+    return ( 
+        <div>
+        <Alert variant="dark" ><Alert.Heading>Hey, Welcome To Our Shop <strong>MyStore</strong></Alert.Heading>
+        <p>Thank you for choosing our store, we hope you enjoy your shopping experience!</p></Alert>
+        <h1>Products</h1>
+        <Row>
+{ProductsList.map((prod,i)=>(<Col key={i}><Product  product={prod}></Product></Col>))}
+</Row>
+    </div>
+
+     );
 }
+ 
+export default Products;
+
