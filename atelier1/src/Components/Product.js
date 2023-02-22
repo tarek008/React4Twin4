2311a4cd31d1901a6f069bd9988acd3a614d0622
tarek008 <tarek.ayadi@esprit.tn>
2023-02-22 23:51:46 +0100

    new update

--- atelier1/src/Components/Product.js
+++ atelier1/src/Components/Product.js
@@ -1,25 +1,63 @@
-import { Component } from "react";
+import { Component, useEffect } from "react";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import { Alert } from "react-bootstrap";
+import { Alert, useAccordionButton } from "react-bootstrap";
 import React from "react";
 
 const Product = ({product}) => {
 
 
   const [Like,SetLike] = React.useState(product.like);
+  const[quantity,setQuantity]=React.useState(product.quantity);
+  const [isBestProduct, setIsBestProduct] = React.useState(false);
+
+  const bestProductStyle = `
+    .bestProduct {
+      background-color: #DB7090;
+      animation: clignote 2s linear infinite;
+    }
+
+    @keyframes clignote {
+      50% {
+        opacity: 0.5;
+      }
+    }
+  `;
 
 
   const handleLike = () => {
     SetLike(Like+1);
+    if(Like>=5){
+      setIsBestProduct(true);
+    }
   }
+
+  const handleQuantity = () => {
+    setQuantity(quantity-1);
+  }
+
+  const[alertVisible,setAlertVisible]=React.useState(false);
+
+
+  const handleVisible = () => { 
+    setAlertVisible(true)
+      setTimeout(() => { 
+        setAlertVisible(false)
+    }, 2000);
+} 
+
+useEffect(()=>{
+  handleVisible()
+},
+[quantity]
+)
 
 
   return ( 
     <div>
 
 
-            <Card style={{ width: '25rem',minHeight:'630px' }}>
+            <Card style={{ width: '25rem',minHeight:'630px' }} >
             <Card.Img variant="top" src={require('../assets/images/'+product.image)} />
             <Card.Body>
               <Card.Title>{product.name}</Card.Title>
@@ -30,9 +68,12 @@ const Product = ({product}) => {
                 {product.price} DT
               </Card.Text>
               <Button variant="primary" onClick={handleLike} >Like</Button> <small>Likes: {Like}</small>
-              <Button variant="danger" >Buy</Button> <small> Products</small>
+              <Button variant="danger" onClick={handleQuantity}>Buy</Button> <small> {quantity}</small>
             </Card.Body>
-            <Alert variant="primary"  >you bought an Item !!!!</Alert>    
+               
+            {alertVisible?<Alert variant="primary" >you bought an Item !!!!</Alert>:false} 
+
+
           </Card>
          
     </div>
@@ -40,3 +81,6 @@ const Product = ({product}) => {
 }
  
 export default Product;
+
+
+
